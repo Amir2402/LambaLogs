9a6041b959ff92c2f6ed4aa67ea536c2caaf30c0
amir <ourratn12@gmail.com>
2025-01-24 18:48:26 +0100

    modified network + producer + launch script

--- input/logProducer.py
+++ input/logProducer.py
@@ -1,2 +1,28 @@
-from sys import stdin 
-from kafka import producer
+from sys import stdin
+from kafka import KafkaProducer 
+import json 
+
+BROKERS = ['kafka-borker-1:9092', 'kafka-broker-2:9093'] 
+TOPIC_NAME = "logTopic" 
+
+def create_Producer(brokers):
+    logProducer = KafkaProducer(bootstrap_servers = brokers, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+    return logProducer
+
+def readLogs(brokers): 
+    logProducer = create_Producer(brokers) 
+
+    try: 
+        for line in stdin: 
+            record = json.loads(line)
+            
+            if record["method"] in ("POST", "GET"): 
+                logProducer.send(topic = TOPIC_NAME, key = "0".encode(), value = record, partition = 0)
+            
+            else: 
+                logProducer.send(topic = TOPIC_NAME, key = "1".encode(), value = record, partition = 1)
+    except: 
+        print("Stream ended or error occured") 
+
+readLogs(BROKERS) 
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -5,11 +5,13 @@ containers=$(sudo docker-compose -f "$COMPOSE_FILE" ps -q)
 
 if [ -n "$containers" ]; then 
     echo "containers are up"; 
+    sudo docker-compose -f "$COMPOSE_FILE" restart
 
 else
     echo "launching containers"; 
     sudo docker-compose -f "$COMPOSE_FILE" up -d 
 fi 
+sleep 10 
 
 TOPIC_NAME="logTopic"
 kafka_topics=$(sudo docker exec -it kafka-broker-1 kafka-topics.sh --bootstrap-server localhost:9092 --list | grep "$TOPIC_NAME")
@@ -23,3 +25,9 @@ else
     --create --bootstrap-server kafka-broker-1:9092,kafka-broker-2:9093 \
     --replication-factor 2 --partitions 2 --topic logTopic
 fi 
+
+../input/flog -l -d 1 -f json | python ../input/logProducer.py & 
+ProcessID="$!"
+
+echo "hey"
+echo "$ProcessID"
