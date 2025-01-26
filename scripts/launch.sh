114964f59d8e9ad9ca2aacb2adc7d31336fedb57
amir <ourratn12@gmail.com>
2025-01-26 16:20:01 +0100

    started working on kafka-connect

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -15,15 +15,15 @@ fi
 sleep 10 
 
 TOPIC_NAME="logTopic"
-kafka_topics=$(sudo docker exec -it kafka-broker-1 kafka-topics.sh --bootstrap-server localhost:9092 --list | grep "$TOPIC_NAME")
+kafka_topics=$(sudo docker exec -it kafka-broker-1 kafka-topics --bootstrap-server localhost:9092 --list | grep "$TOPIC_NAME")
 
 if [ -n "$kafka_topics" ]; then 
     echo "Topic exists"; 
 
 else
     echo "Creating topic"; 
-    sudo docker exec -it kafka-broker-1 kafka-topics.sh \
-    --create --bootstrap-server kafka-broker-1:9092,kafka-broker-2:9093 \
+    sudo docker exec -it kafka-broker-1 kafka-topics \
+    --create --bootstrap-server kafka-broker-1:9092,kafka-broker-2:9093,kafka-broker-3:9094 \
     --replication-factor 2 --partitions 2 --topic logTopic
 fi 
 
